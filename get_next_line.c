6af4508f0cc0969e6dbd779884afa5b5b1b84aae
nano7767 <126381073+nano7767@users.noreply.github.com>
2023-04-08 14:12:30 +0700

    Add files via upload

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: svikornv <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/05 17:02:52 by svikornv          #+#    #+#             */
-/*   Updated: 2023/04/05 17:40:48 by svikornv         ###   ########.fr       */
+/*   Updated: 2023/04/08 12:33:17 by svikornv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,7 +39,7 @@ int	contain_nl(t_list *stash)
 	return (0);
 }
 
-//free stash up to '\n'
+//free stash up to '\n' including '\n'
 void	free_stash(t_list **stash)
 {
 	t_list *tmp;
@@ -49,31 +49,26 @@ void	free_stash(t_list **stash)
 
 	//make new tmp node
 	tmp = malloc(sizeof(t_list));
-	if (tmp == NULL)
+	if (stash == NULL || tmp == NULL)
 		return ;
 	tmp->next = NULL;
-	//we know latest node contains '\n' so pointer to last node
-	ptr = ft_lstlast(*stash);
-	i = 0;
 	j = 0;
-	//get the position after '\n'
-	while (ptr->content[i] != '\n')
+	i = 0;
+	ptr = ft_lstlast(*stash);
+	while (ptr->content[i]  && ptr->content[i] != '\n')
 		i++;
+	if (ptr->content  && ptr->content[i] == '\n')
 	i++;
 	tmp->content = (char *)malloc(sizeof(char) * ((ft_strlen(ptr->content) - i) + 1));
-	if (ptr->content == NULL)
+	if (tmp->content == NULL)
 		return ;
 	//the content of tmp node is now the content of the last node after new line
 	while (ptr->content[i])
-	{
-		tmp->content[j] = ptr->content[i];
-		j++;
-		i++;
-	}
+		tmp->content[j++] = ptr->content[i++];
 	//null terminate tmp
 	tmp->content[j] = '\0';
 	//free stash and copy tmp to stash
-	clear_list(stash);
+	clear_list(*stash);
 	*stash = tmp;
 }
 
@@ -112,24 +107,29 @@ void	add_to_stash(t_list **stash, char *buf, int read_size)
 char	*extract_line(t_list *stash)
 {
 	char	*line;
-	int	line_len;
+	//int	line_len;
 	int	i;
 	int	j;
 	t_list	*ptr;
 
 	//allocate size enough for stash
+	/*
 	line_len = BUFFER_SIZE * (ft_lstsize(stash) - 1) + last_node_len(stash);
 	line = (char *)malloc(sizeof(char) * (line_len + 1));
+	*/
+	line = (char *)malloc(sizeof(char) * (total_node_len(stash) + 1));
 	if (line == NULL)
 		return (NULL);
 	i = 0;
 	j = 0;
 	ptr = stash;
-	while (1)
+	while (ptr)
 	{
+		/*
 		//if end of line
 		if (ptr == NULL)
 			break ;
+		*/
 		//while the content of the node is not end
 		while (ptr->content[j])
 		{
@@ -162,7 +162,7 @@ char	*get_next_line(int fd)
 	int	read_size;
 
 	//if invalid file or BUFFER_SIZE return NULL
-	if (fd < 0 || fd == 0 || BUFFER_SIZE <= 0 || read(fd, &line, 0))
+	if (fd < 0 || fd == 0 || BUFFER_SIZE <= 0 || read(fd, &line, 0) < 0)
 		return (NULL);
 	line = NULL;
 	read_size = 0;
@@ -175,31 +175,30 @@ char	*get_next_line(int fd)
 			return (NULL);
 		//read continuously
 		read_size = read(fd, buf, BUFFER_SIZE);
-		//null terminate buf
-		buf[BUFFER_SIZE] = '\0';
 		//if end of file
 		if (read_size <= 0 && stash != NULL)
 		{
 			free(buf);
-			buf = NULL;
 			//extract line from stash
 			line = extract_line(stash);
-			clear_list(&stash);
+			clear_list(stash);
 			//break off loop to return line
 			break ;
 		}
+		//if empty file, return
+		else if (read_size <= 0 && stash == NULL)
+			return (NULL);
+		//null terminate buf
+		buf[read_size] = '\0';
 		//store buf to stash as linked list
 		add_to_stash(&stash, buf, read_size);
 		//free buf after storing
 		free(buf);
-		//if empty file, return
-		if (stash == NULL)
-			return (NULL);
 		if (contain_nl(stash) == 1)
 		{
 			//if new line character is found, extract line from stash up to '\n'
 			line = extract_line(stash);
-			//free stash up to new line function;
+			//free stash until after '\n'
 			free_stash(&stash);
 			//break to return the line;
 			break ;
@@ -209,7 +208,7 @@ char	*get_next_line(int fd)
 		{
 			if (stash != NULL)
 			{
-				clear_list(&stash);
+				clear_list(stash);
 				stash = NULL;
 			}
 			free(line);
@@ -218,6 +217,8 @@ char	*get_next_line(int fd)
 		}
 	return (line);
 }
+
+//PROBLEM FREE NOT ALLOCATE WHEN SINGLE CHARACTER LIKE "\n"
 /*
 #include <fcntl.h>
 int main()
